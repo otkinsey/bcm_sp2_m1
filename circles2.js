ac29f9d41e66a9bb8c65b1b466717c2735f13860
okoa kinsey <okoakinsey@okoas-MacBook-Pro.local>
2019-03-25 14:58:25 -0400

    canvas number variable has been updated. Further testing required

--- circles2.js
+++ circles2.js
@@ -10,13 +10,14 @@ function init(){
         body.style = `height:100vh`;
         var context;
         var canvasArray = [];
+        var canvasNumber = (canvasArray.length) ? Math.max(...canvasArray) : 1;
 
         // randomize hexadecimal string for fill color
         function randomizeColor(){
             
             var possible = ["#1e69e2","#e21ed9","#39e21e","#f180eb","#9c27b0"];
             hexString = possible[Math.floor((Math.random()/4)*10)];  
-            console.log("DIAG: circle.js - randomizeColor: "+hexString);
+            // console.log("DIAG: circle.js - randomizeColor: "+hexString);
             return hexString;          
         }
 
@@ -39,19 +40,18 @@ function init(){
             canvas.style = `border:1px dotted grey;position:absolute;left:${canvasX}px;top:${canvasY}px;`;
             
             // set id of new canvas and add to canvasArray
-            // canvas.id = `${canvasArray.length+1}`;
-            var canvasNumber = (canvasArray.length) ? Math.max(...canvasArray) : 0;            
+                        
             var canvasRatio = canvas.offsetLeft/canvas.offsetTop; 
-            canvasArray.push(canvas.id);
+            canvas.id = `${canvasNumber}`;
+
+            canvasArray.push(canvasNumber);
             e.target.appendChild(canvas);
             canvas.setAttribute('ratio', canvasRatio);
            
-            console.log("canvasNumber: "+canvasNumber);
-            
-            
-            canvasNew = document.getElementById(canvas.id);
+            // console.log("canvasNumber: "+canvasNumber);
+                        
+            canvasNew = document.getElementById(canvasNumber);
             context = canvasNew.getContext("2d");
-
 
             // an array to save radius values based on 
             context.fillStyle = randomizeColor();
@@ -63,13 +63,19 @@ function init(){
                 
                 var canvases = document.getElementsByTagName("canvas");
                 var currentCanvas = document.getElementById(canvasArray[canvasArray.length-1]);
-                document.getElementById(i).removeAttribute('current');
+                console.log("DIAG circle2.js - array contents: "+canvasArray);
+                // document.getElementById(currentCanvas).removeAttribute('current');
+                console.log("DIAG: circles.js - i: "+i);
+                if(canvasArray.includes(i)){
+                    document.getElementById(i).removeAttribute('current');
+                }
+
                 currentCanvas.setAttribute("current",true);
-                // console.log("DIAG: circles.js - i: "+i);
+               
                 
                 var currentX = currentCanvas.offsetLeft;
                 var currentY = currentCanvas.offsetTop;
-                console.log("Diag - current canvas x: "+currentCanvas.offsetLeft+" y: "+currentCanvas.offsetTop);
+                // console.log("Diag - current canvas x: "+currentCanvas.offsetLeft+" y: "+currentCanvas.offsetTop);
                 for(c of canvases){   
                     if(c.id == currentCanvas.id){
                         console.log('skipping');
@@ -86,21 +92,28 @@ function init(){
                         console.log(`currentCanvas: ${currentCanvas.id} overlaps with ${c.id}`);
 
                         // delete over lapping canvas
-                        canvasArray.splice(canvasArray.indexOf(c.id),1);
-                        console.log("canvas id: "+c.id+" index: "+canvasArray.indexOf(c.id));
+                        console.log("[ DIAG - circles.js ] c.id: "+c.id);
+                        console.log(c);
+                        canvasArray.splice((c.id-1),1);
+                        console.log("[ DIAG - circles2.js ] canvasArray: "+canvasArray);
+                        // console.log("canvas id: "+c.id+" index: "+canvasArray.indexOf(c.id));
+                        console.log("[ DIAG circles.js ] array content: "+canvasArray);
                         body.removeChild(c);   
-                        canvasNumber = Math.max(...canvasArray);
-                        console.log("number: "+canvasNumber);                    
+                        canvasNumber = Math.max(...canvasArray)+1;
+                        console.log("number: "+parseInt(canvasNumber+1));                    
                     }
-
+                    // console.log("canvas id: "+c.id+" index: "+canvasArray.indexOf(c.getAttribute('id')));
                 }
                 
             })
 
+            
             context.fill();
             context.closePath();    
-            console.log("canvasArray: "+canvasArray);   
+            console.log("canvasArray: "+canvasArray);  
+            return canvasNumber += 1; 
     }
+    
 }
 
 
